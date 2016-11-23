03e6775fff36ed935403fc0e7c038735b40a755b
cbam <756028571@qq.com>
2016-11-23 19:09:46 +0800

    新增通用Mapper、配置mybatis分页插件

---
+++ idoc-sso/src/main/java/com/swagger/idoc/service/getDocInfoByIdService.java
@@ -0,0 +1,17 @@
+package com.swagger.idoc.service;
+
+import com.swagger.idoc.po.TDocInfo;
+
+/**
+ * CopyRright (c)2014-2016 Haerbin Hearglobal Co.,Ltd
+ * Project: idoc-main
+ * Comments:
+ * Author:cbam
+ * Create Date:2016/11/20
+ * Modified By:
+ * Modified Date:
+ * Modified Reason:
+ */
+public interface getDocInfoByIdService {
+    TDocInfo getDocInfoById();
+}
